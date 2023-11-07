44a8ad95cca75181b8066009ed618a4a4a4528a8
nthippar <147669970+nthippar@users.noreply.github.com>
2023-11-06 20:32:06 -0600

    demo 21

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,65 +1,68 @@
-import streamlit as st
-from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
-from hugchat import hugchat
+# import streamlit as st
+# from streamlit_chat import message
+# from streamlit_extras.colored_header import colored_header
+# from streamlit_extras.add_vertical_space import add_vertical_space
+# from hugchat import hugchat
 from request import requests
 
-
-st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
-
-# Sidebar contents
-with st.sidebar:
-    st.title('NS Quiz Bot')
-    st.markdown('''
-    ## About
-    This app is an LLM-powered quiz built using:
-    - [Streamlit](https://streamlit.io/)
-    - [HugChat](https://github.com/Soulter/hugging-chat-api)
-    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
-    
-    💡 Note: No API key required!
-    ''')
-    add_vertical_space(5)
-    st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
-
-# Generate empty lists for generated and past.
-## generated stores AI generated responses
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
-## past stores User's questions
-if 'past' not in st.session_state:
-    st.session_state['past'] = ['Hi!']
-
-# Layout of input/response containers
-input_container = st.container()
-colored_header(label='', description='', color_name='blue-30')
-response_container = st.container()
-
-# User input
-## Function for taking user provided prompt as input
-def get_text():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
-## Applying the user input box
-with input_container:
-    user_input = get_text()
-
-# Response output
-## Function for taking user prompt as input followed by producing AI generated responses
-def generate_response(prompt):
     response = requests.get("https://api.open-notify.org/this-api-doesnt-exist")
-    # response = chatbot.chat(prompt)
-    return response.status_code
 
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
+
+
+# st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+
+# # Sidebar contents
+with st.sidebar:
+    st.title('response.status_code')
+#     st.markdown('''
+#     ## About
+#     This app is an LLM-powered quiz built using:
+#     - [Streamlit](https://streamlit.io/)
+#     - [HugChat](https://github.com/Soulter/hugging-chat-api)
+#     - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
+    
+#     💡 Note: No API key required!
+#     ''')
+#     add_vertical_space(5)
+#     st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
+
+# # Generate empty lists for generated and past.
+# ## generated stores AI generated responses
+# if 'generated' not in st.session_state:
+#     st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+# ## past stores User's questions
+# if 'past' not in st.session_state:
+#     st.session_state['past'] = ['Hi!']
+
+# # Layout of input/response containers
+# input_container = st.container()
+# colored_header(label='', description='', color_name='blue-30')
+# response_container = st.container()
+
+# # User input
+# ## Function for taking user provided prompt as input
+# def get_text():
+#     input_text = st.text_input("You: ", "", key="input")
+#     return input_text
+# ## Applying the user input box
+# with input_container:
+#     user_input = get_text()
+
+# # Response output
+# ## Function for taking user prompt as input followed by producing AI generated responses
+# def generate_response(prompt):
+    # response = requests.get("https://api.open-notify.org/this-api-doesnt-exist")
+#     # response = chatbot.chat(prompt)
+#     return response.status_code
+
+# ## Conditional display of AI generated responses as a function of user provided prompts
+# with response_container:
+#     if user_input:
+#         response = generate_response(user_input)
+#         st.session_state.past.append(user_input)
+#         st.session_state.generated.append(response)
         
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+#     if st.session_state['generated']:
+#         for i in range(len(st.session_state['generated'])):
+#             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+#             message(st.session_state["generated"][i], key=str(i))
